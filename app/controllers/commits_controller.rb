1b7b6395287fc4a6ab8285be6dd5317610556ed5
ramazan yaraliev <yaru@bk.ru>
2018-12-18 22:25:43 +0300

    add flash messages; edit #import

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -7,25 +7,35 @@ class CommitsController < ApplicationController
   end
 
   def import
-    owner = params[:owner]
-    repo = params[:repo]
-    author_email = params[:author_email]
+    if params.has_key?(:owner) && params.has_key?(:repo) && params.has_key?(:author_email)
+      owner = params[:owner]
+      repo = params[:repo]
+      author_email = params[:author_email]
+      client = Octokit::Client.new()
 
-    client = Octokit::Client.new()
-
-    unless owner.nil? and repo.nil? and author_email.nil?
-      client.list_commits("#{owner}/#{repo}")
-      last_response = client.last_response
-      messages = []
-      messages.concat(messages_from_response(last_response, author_email))
-
-      until last_response.rels[:next].nil?
-        last_response = last_response.rels[:next].get
+      begin
+        client.list_commits("#{owner}/#{repo}")
+        last_response = client.last_response
+        messages = []
         messages.concat(messages_from_response(last_response, author_email))
-      end
 
-      save(owner, repo, author_email, messages) if messages.any?
+        until last_response.rels[:next].nil?
+          last_response = last_response.rels[:next].get
+          messages.concat(messages_from_response(last_response, author_email))
+        end
+
+        if messages.any?
+          flash[:notice] = save?(owner, repo, author_email, messages) ? 'Commits successfully imported' : "Commits doesn't imported"
+        else
+          flash[:notice] = 'Commits not found'
+        end
+
+      rescue Octokit::NotFound
+        flash[:notice] = "#{owner}/#{repo} not found"
+      end
     end
+
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
@@ -41,16 +51,20 @@ class CommitsController < ApplicationController
     messages
   end
 
-  def save(owner_name, repo_name, email, messages)
+  def save?(owner_name, repo_name, email, messages)
     owner = Owner.where(name: owner_name).first_or_create
     repo = owner.repos.where(name: repo_name).first_or_create
     author_email = repo.author_emails.where(email: email).first_or_create
-
     commits_old = repo.commits.where(author_email: author_email)
 
-    commits_old.transaction do
-      commits_old.destroy_all
-      author_email.commits.create(messages)
+    begin
+      commits_old.transaction do
+        commits_old.destroy_all
+        author_email.commits.create(messages)
+        return true
+      end
+    rescue ActiveRecord::StatementInvalid
+      return false
     end
   end
 end
